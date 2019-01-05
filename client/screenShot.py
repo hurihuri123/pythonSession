5f72aadeb0dc722a89d0840f58c3239526525822
danielhuri <danielhuri8@gmail.com>
2019-01-05 16:13:36 +0200

    show image in Tkinter - DONE

--- client/screenShot.py
+++ client/screenShot.py
@@ -1,14 +1,21 @@
+
+from StringIO import StringIO
+from Tkinter import *
 from PIL import Image
 from PIL import ImageTk
-from StringIO import StringIO
-
 # Read and  open screen shot
 class ScreenShot:
     def __init__(self):
         pass
 
     def showImage(self, data):
+        # Convert data to image
         img = Image.open(StringIO(data))
-        img.save("img.jpeg","JPEG")
+        # img.save("img.jpeg","JPEG") # Save image to file
 
-        # photo = ImageTk.PhotoImage(img)
+        # Open image in Tkinter
+        root = Tk()
+        imgTk = ImageTk.PhotoImage(img)
+        panel = Label(root, image=imgTk)
+        panel.pack(side="bottom", fill="both", expand="yes")
+        root.mainloop()
